d7ae2d5c349ddc976dfbfb8b5a9eb0af9347cad9
Louis CAPELLE <louiscapelle@icloud.com>
2020-11-09 10:13:42 +0100

    creation of users has been corrected

--- migrations/Version20201109091017.php
+++ migrations/Version20201109091017.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20201105134025 extends AbstractMigration
+final class Version20201109091017 extends AbstractMigration
 {
     public function getDescription() : string
     {
